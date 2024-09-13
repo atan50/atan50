0df77d65cf69a616cd0d39d9f0ea0de4dc4c926e
Amanda Tan <atan50@stuy.edu>
2024-09-13 11:01:35 -0400

    started 04_rando-dict

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,28 @@
+#Amanda Tan
+#Elmo's Cheez-it
+#SoftDev
+#K04 -- Python dictionairies and random selection
+#2024-09-13
+#time spent:
+
+import random
+
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+ranNum = random.randint(0, len(krewes)-1)
+print(ranNum)
+nameList = krewes.get(ranNum)
+print(nameList)
