31bb817b9cfa89b82cdf82064ef02ba6d077a3f9
Amanda Tan <atan50@stuy.edu>
2024-10-16 11:01:14 -0400

    styled CSS with flask

---
+++ 18_live-stuyle/app.py
@@ -0,0 +1,18 @@
+"""
+    Elmos_Cheez-its: Amanda Tan, Ethan Sie, Aidan Wong
+    SoftDev
+    K18 - CSS Styling with Flask
+    2024-10-16
+    time spent: .2
+"""
+
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def disp_home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
