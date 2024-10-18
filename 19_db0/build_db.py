8781ae039f0bc07cac9b56c00065600bfbddf187
Amanda Tan <atan50@stuy.edu>
2024-10-18 11:01:44 -0400

    read cvs file and played with sqlite3

---
+++ 19_db0/build_db.py
@@ -0,0 +1,40 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev
+#skeleton/stub :: SQLITE3 BASICS
+#Oct 2024
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+#==========================================================
+
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+< < < INSERT YOUR TEAM'S DB-POPULATING CODE HERE > > >
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+def readTable():
+    with open("courses.csv") as file:
+        data = file.read().strip().split("\n")
+
+    for _ in data:
+        _ = _.split(',')
+        print(_)
+
+print(readTable())
+
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
