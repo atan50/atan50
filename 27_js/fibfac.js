b60ba03d0874b9f4b83851b88806920cf5b05846
Amanda Tan <atan50@stuy.edu>
2025-01-07 11:17:17 -0500

    upload 27 base code

---
+++ 27_js/fibfac.js
@@ -0,0 +1,31 @@
+//Team MagiMan :: Amanda Tan, Kishi Wijaya
+//SoftDev pd4
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+
+var fact = function(x){
+  if(n == 1){
+    return n;
+  }
+  return (n*fact(n-1))
+}
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+
+//-----------------------------------------------------------------
+
+
+//fib:
+
+//<your team's fib(n) implementation>
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+
+//=================================================================
